e58c665cd569db1b1a2becbfbabdba33e5e4cdef
The Bipu <127449591+the-bipu@users.noreply.github.com>
2026-07-21 22:31:00 +0530

    Update next.config.mjs

--- frontend-next/next.config.mjs
+++ frontend-next/next.config.mjs
@@ -1,29 +1,25 @@
-// next.config.mjs
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Your Next.js configuration
-    images: {
-        remotePatterns: [
-            {
-            protocol: "https",
-            hostname: "images.pexels.com",
-            },
-            {
-            protocol: "https",
-            hostname: "images.unsplash.com",
-            },
-            {
-            protocol: "https",
-            hostname: "media.licdn.com",
-            },
-            {
-            protocol: "https",
-            hostname: "i.ibb.co",
-            },
-        ],
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+    ],
+  },
 };
 
-const defaultLocale = nextConfig.i18n.defaultLocale;
-
-export { nextConfig as default, defaultLocale };
+export default nextConfig;
